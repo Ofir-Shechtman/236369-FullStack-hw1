5e0b8eca8c75792bb79f8a92e35995eed63d4b39
Ofir Shechtman <ofir511996@gmail.com>
2021-11-04 10:44:34 +0200

    Add files

---
+++ hw1_template.py
@@ -0,0 +1,18 @@
+import socket
+import os
+import string
+import hw1_utils
+from pdfminer import high_level
+
+# Define socket host and port
+SERVER_HOST = '127.0.0.1'
+SERVER_PORT = 8888
+
+
+
+
+
+if __name__ == "__main__":	
+    pass
+    while True:
+        pass
